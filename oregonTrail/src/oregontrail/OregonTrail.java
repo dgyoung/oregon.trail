648762954b62e48902b233e027682c9086ed4009
davey <davey@NCC1280>
2018-01-30 19:29:32 -0700

    test

--- oregonTrail/src/oregontrail/OregonTrail.java
+++ oregonTrail/src/oregontrail/OregonTrail.java
@@ -16,6 +16,8 @@ public class OregonTrail {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //just to show how it works
     }
     
 }
