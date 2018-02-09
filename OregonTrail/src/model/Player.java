40bf5a957df3142e538c5cffe45908e892200bab
bradrallen <bradrallen@localhost>
2018-02-08 21:20:16 -0700

    Oregon Trail Game

--- OregonTrail/src/model/Player.java
+++ OregonTrail/src/model/Player.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package oregontrail;
+package model;
 
 import java.util.Objects;
 import java.io.Serializable;
