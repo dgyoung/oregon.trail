80dc120af0a595a161ab71ccc617b059df2e3a0e
ninjawriter222 <devin.a.poe@gmail.com>
2018-02-08 20:21:19 -0700

    Learning to Push

---
+++ JavaApplication4/src/Control/InventoryControl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Model.StorageBox;
+/**
+ *
+ * @author MasterCraft Computer
+ */
+public class InventoryControl 
+{
+calcVolumeOfStorageBox(height,width,depth)double  
+{
+    if (height < 0)
+    {
+        return -1;
+    }
+    if (width < 0)
+    {
+        return -1;
+    }
+    if (depth < 0)
+    {
+        return -1;
+    }
+}
+}
--- JavaApplication4/src/Model/Main.java
+++ JavaApplication4/src/Model/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication4;
+package Model;
 
 /**
  *
  * @author MasterCraft Computer
  */
-public class JavaApplication4 {
+public class Main {
 
     /**
      * @param args the command line arguments
