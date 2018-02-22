d3d0d547cc36adada8ba8f90c33803cf7fd61897
David <David@QWERTYking-8>
2018-02-21 20:19:44 -0700

    started view layer 02/21/18

--- oregonTrail/src/byui/cit260/oregontrail/view/StartProgramView.java
+++ oregonTrail/src/byui/cit260/oregontrail/view/StartProgramView.java
@@ -1,13 +1,50 @@
 package byui.cit260.oregontrail.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alexandra
  */
 public class StartProgramView {
-    
+
     public StartProgramView() {
+
+    }
+
+    public void displayStartProgramView() {
+        boolean endOfView = false;
+        String[] inputs = this.getInputs();
+        do {
+            if (inputs[0] == null || inputs[0].equals( '6' )) {//IF no inputs were entered OR the first input is Q
+                return;
+            }
+            if(endOfView = doAction(inputs))
+                endOfView = true;
+            //ENDIF endOfView = doAction(inputs)
+        }while (endOfView == false); //WHILE endOfView   != true ;
         
+    
+    }
+
+    private String[] getInputs() {
+        String[] inputs = new String[1];
+        
+        System.out.println("this is a description of the view");
+        
+        boolean valid = false;
+        while (valid == false) {
+            System.out.println("enter in a character: ");
+            
+        }
+        return inputs;
     }
     
+    private boolean doAction(String[] inputs){
+        System.out.println("**** doAction() called ****");
+        System.out.println("\tinputs = " + inputs[0]);
+        
+        return true;
+    }
+
 }
--- oregonTrail/src/oregontrail/OregonTrail.java
+++ oregonTrail/src/oregontrail/OregonTrail.java
@@ -15,6 +15,7 @@ import byui.cit260.oregontrail.model.Wagon;
 import byui.cit260.oregontrail.model.Map;
 import byui.cit260.oregontrail.model.Location;
 import byui.cit260.oregontrail.control.GameControl;
+import byui.cit260.oregontrail.view.StartProgramView;
 
 /**
  *
@@ -26,55 +27,8 @@ public class OregonTrail {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         Game game1 = new Game();
-         game1.setNoPeople(10);
-         game1.setTotalDays(4040);
-         System.out.println(game1.toString());
-         System.out.print(game1.getNoPeople() + " ");
-         System.out.println(game1.getTotalDays());
-         
-         RiverScene.Columbia.getFerryCost();
-         System.out.print(RiverScene.Columbia.getDepth() + " $");
-         System.out.println(RiverScene.Columbia.getFerryCost());
-         System.out.println(HuntingScene.bear.toString() + " \n" + 
-                 HuntingScene.elk.toString() + " \n" + 
-                 HuntingScene.squirrel.toString() + " \n" + 
-                 HuntingScene.buffalo.toString() + " \n" + 
-                 HuntingScene.rabbit.toString() + " \n" + 
-                 HuntingScene.deer.toString());
-         System.out.println(SceneDescription.huntingScene.toString() + " \n" +
-                 SceneDescription.riverScene.toString() + " \n" +
-                 SceneDescription.storeScene.toString());
-         System.out.println(StoreScene.store1.toString() + " \n" +
-                 StoreScene.store2.toString() + " \n" +
-                 StoreScene.store3.toString() + " \n" +
-                 StoreScene.store4.toString() + " \n" +
-                 StoreScene.store5.toString());
-        
-        Character character1 = new Character();
-        character1.setName("Kolob");
-        character1.setAffliction("Cholera");
-        System.out.println(character1.toString());
-        
-        Wagon wagon1 = new Wagon();
-        wagon1.setPace(3);
-        wagon1.setRations("Meager");
-        System.out.println(wagon1.toString());
-        
-        Map map1 = new Map();
-        map1.setPathChoice("Whichever has the most snakes?");
-        map1.setCheckpoint("You've reached the Trading Post in Missouri");
-        System.out.println(map1.toString());
-        
-        Location location1 = new Location();
-        location1.setLabel("OREGON!");
-        location1.setNext(235);
-        location1.setPosition(564);
-        location1.setEnd(36);
-        location1.setWeather("Monsoon");
-        System.out.println(location1.toString());
-        int fun = GameControl.getFinalScore(5000, 3, 6);
-        System.out.println(fun);
+         StartProgramView startProgramView = new StartProgramView();
+         startProgramView.displayStartProgramView();
        
     }
     
