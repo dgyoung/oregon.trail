9b89a3237152e8715b802091032b78776cc40f3d
Glaser-Allie <gla13006@byui.edu>
2018-02-24 03:05:07 -0600

    added MainMenuView.java

---
+++ oregonTrail/src/byui/cit260/oregontrail/view/MainMenuView.java
@@ -0,0 +1,13 @@
+package byui.cit260.oregontrail.view;
+
+/**
+ *
+ * @author Alexandra
+ */
+class MainMenuView {
+
+    void displayMainMeuView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
