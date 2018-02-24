67bd8a865854d2a42baa4c341f4886e2498042e9
Glaser-Allie <gla13006@byui.edu>
2018-02-24 02:45:16 -0600

    minor text edits

--- oregonTrail/src/oregontrail/OregonTrail.java
+++ oregonTrail/src/oregontrail/OregonTrail.java
@@ -15,13 +15,9 @@ import byui.cit260.oregontrail.model.Wagon;
 import byui.cit260.oregontrail.model.Map;
 import byui.cit260.oregontrail.model.Location;
 import byui.cit260.oregontrail.control.GameControl;
-
 import byui.cit260.oregontrail.view.StartProgramView;
 
-/**
- *
- * @author davey, allie, &
- */
+
 public class OregonTrail {
 
     /**
