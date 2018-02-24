df06648817c897a895e438284db3e50aa85685cb
Glaser-Allie <gla13006@byui.edu>
2018-02-24 02:44:07 -0600

    changes to doAction

--- oregonTrail/src/byui/cit260/oregontrail/view/StartProgramView.java
+++ oregonTrail/src/byui/cit260/oregontrail/view/StartProgramView.java
@@ -1,6 +1,9 @@
 package byui.cit260.oregontrail.view;
 import byui.cit260.oregontrail.control.GameControl;
 import static byui.cit260.oregontrail.control.GameControl.setCharactersNames;
+import byui.cit260.oregontrail.model.Player;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.Scanner;
 
@@ -11,40 +14,63 @@ import java.util.Scanner;
 public class StartProgramView {
 
     public StartProgramView() {
-
+        System.out.println(
+                "\n*****************************************************************"
+                + "\n"
+                + "\n The Oregon Trail game is a text based role playing game."
+                + "\n You'll take on the role of leader of a wagon party and"
+                + "\n everyone in your wagon is counting on you to keep them alive."
+                + "\n"
+                + "\n You will need to prepare to travel through plains, rivers,"
+                + "\n and mountains as you make your way to Oregon."
+                + "\n Resources will be scarce, and you will encounter challenges,"
+                + "\n including disease, river crossings, thieves, and limited food."
+                + "\n "
+                + "\n But don’t be too intimidated, the scenery is wonderful and"
+                + "\n the challenge is possible – as long as you make it through"
+                + "\n the game with one wagon member alive, you win!"
+                + "\n"
+		+ "\n*****************************************************************");
+        
     }
-
+    
     public void displayStartProgramView() {
         boolean endOfView = false;
         String[] inputs = this.getInputs();
+        
         do {
-            if (inputs[0] == null || inputs[0].equals("6")) {
-                return;
-            }
-            if (endOfView = doAction(inputs)) {
-                endOfView = true;
+            if (inputs[0].toUpperCase().equals("Q")) 
+            {
+                return; //exits program
             }
             //ENDIF endOfView = doAction(inputs)
-        } while (endOfView == false); //WHILE endOfView   != true ;
+            endOfView = this.doAction(inputs);
+            
+        } while (!endOfView); //WHILE endOfView   != true ;
 
     }
 
     private String[] getInputs() {
+
         String[] inputs = new String[1];
 
-        System.out.println("this is a description of the view");
+        
         
         Scanner scanner = new Scanner(System.in);
         String userInput;
         String trimmedUserInput = null;
         boolean valid = false;
-        while (valid == false) {
-            System.out.println("enter in a character: ");
+        while (!valid) {
+            
+            System.out.println("\n" + "Please enter your name: ");
+            
             userInput = scanner.nextLine();
             trimmedUserInput = userInput.trim();
+            
             if (trimmedUserInput.length() < 1) {
                 System.out.println("You must enter a non-blank value");
             }
+            
             else {
                 valid = true;
             }
@@ -53,15 +79,38 @@ public class StartProgramView {
         return inputs;
     }
 
-    private boolean doAction(String[] inputs) {
-        
-        if (setCharactersNames(inputs[0])){
-            return false;
+    private String doAction(String[] inputs) {
+        //playersName = get the first value in the inputs array
+        String playersName;
+        playersName = inputs(0);
+        //player = savePlayer(playersName)
+        Player player = new GameControl.saveGame(playersName);
+        //IF player == null
+        if (playersName == null) {
+                System.out.println("Could not create the player." + "\n" +
+               "Enter a different name.");
+           
+            return null;
         }
+        else {
+                System.out.println(
+                "================================================="
+                + "Welcome to the game " + playersName 
+                + "We hope you have a lot of fun!"
+                + "=================================================");
+            }
+         
+        //mainMenuView = Create a new MainMenuView object
+        MainMenuView mainMenuView = new MainMenuView();
         
+        //mainMenuView.displayMainMenuView()
+        mainMenuView.displayMainMeuView();
         
-        
-        return true;
+        return null;      
+    }
+
+    private String inputs(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
