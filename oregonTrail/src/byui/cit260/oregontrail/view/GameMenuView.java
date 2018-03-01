047a576f26f7a21ef553334d36597e04805fbb91
Glaser-Allie <gla13006@byui.edu>
2018-02-28 23:42:52 -0600

    created GameMenuView

--- oregonTrail/src/byui/cit260/oregontrail/view/GameMenuView.java
+++ oregonTrail/src/byui/cit260/oregontrail/view/GameMenuView.java
@@ -7,10 +7,28 @@ package byui.cit260.oregontrail.view;
 
 /**
  *
- * @author David
+ * @author Allie
  */
 class GameMenuView {
     void displayGameMenuView(){
-        System.out.println("*** displayGameMenuView() called ***");
+        
+        System.out.println("\n********** The Oregon Trail *******************"
+                + "* Location: Independence *"
+                + "* Date: March 1, 1848 *"
+                + "* Weather: Cold *"
+                + "* Health: good *"
+                + "* Pace: steady *"
+                + "* Rations: filling *"
+                + "* *"
+                + "* 1. Continue on trail *"
+                + "* 2. Check supplies *"
+                + "* 3. Display the map *"
+                + "* 4. Change pace *"
+                + "* 5. Change food rations *"
+                + "* 6. Stop to rest *"
+                + "* 7. Hunt *"
+                +"*-----What is your choice? *");
+
+        boolean endOfView = false;
     }
 }
