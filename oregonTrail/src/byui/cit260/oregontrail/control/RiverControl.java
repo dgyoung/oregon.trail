7788f862aa58eb39d556d9a5f6575b393189474f
ninjawriter222 <devin.a.poe@gmail.com>
2018-03-24 19:04:47 -0700

    Devin's individual assignment, river.

--- oregonTrail/src/byui/cit260/oregontrail/control/RiverControl.java
+++ oregonTrail/src/byui/cit260/oregontrail/control/RiverControl.java
@@ -5,24 +5,68 @@
  */
 package byui.cit260.oregontrail.control;
 
+import byui.cit260.orgontrail.exceptions.GameControlException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author David
  */
 public class RiverControl {
-    public static String rindCurrentWeather(){
+    
+    public static String findCurrentWeather(){
         return "";
     }
-    public static boolean calculateFording(int riverWidth, int riverDepth){
+    public static boolean calculateFording(int riverWidth, int riverDepth)
+    {
+        int calculateFording = 0;
+        if (calculateFording < 0)
+        {
+        return false;
+        }
+        else
         return true;
     }
-    public static boolean calculateCaulk(int riverWidth, int riverDepth){
+    public static boolean calculateCaulk(int riverWidth, int riverDepth)
+    {
+        int calculateCaulk = 0;
+        if (calculateCaulk < 0)
+        {
+        return false;
+        }
+        else
         return true;
     }
-    public static void findHelpCost(){
+    public static void findHelpCost()
+    {
         
     }
-    public static boolean calculateFerry(){
+    public static boolean calculateFerry()
+    {
+        int calculateFerry = 0;
+        if (calculateFerry < 0)
+        {
+        return false;
+        }
+        else
         return true;
+    }
+    
+    public RiverControl(boolean calculateFording) throws GamecontrolException{
+        if (calculateFording == false) {
+            try {
+                throw new GameControlException("ERROR: Cannot be less than 0");
+                        } catch (GameControlException ex) {
+                Logger.getLogger(RiverControl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }    
+        
+    }
+  
+    
+    private static class GamecontrolException extends Exception {
+
+        public GamecontrolException() {
+        }
     }
 }
