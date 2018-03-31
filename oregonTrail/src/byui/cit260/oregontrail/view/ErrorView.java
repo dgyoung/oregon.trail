781895d1846c66f2a5de7540fa86e470cddc206e
Glaser-Allie <gla13006@byui.edu>
2018-03-31 18:34:40 -0500

    merged differences

--- oregonTrail/src/byui/cit260/oregontrail/view/ErrorView.java
+++ oregonTrail/src/byui/cit260/oregontrail/view/ErrorView.java
@@ -3,23 +3,21 @@ package byui.cit260.oregontrail.view;
 import java.io.PrintWriter;
 import oregontrail.OregonTrail;
 
-/**
- *
- * @author Alexandra
- */
 public class ErrorView {
+  private static PrintWriter console = OregonTrail.getOutFile();
+  private static PrintWriter log = OregonTrail.getLogFile();
 
     private static final PrintWriter errorFile = OregonTrail.getOutFile();
     private static final PrintWriter logFile = OregonTrail.getLogFile();
 
     public static void display(String className, String errorMessage) {
 
-        errorFile.println(
-                "-----------------------------------"
-                + "\n -Error- " + errorMessage
-                + "\n ---------------------------------");
-        // log the error
-        logFile.println(className + " - " + errorMessage);
+        console.println(
+              "\n--- ERROR  --------------------"
+             +"\n" + errorMessage
+             +"\n------------------------------");
+        
+        log.printf("%n%n%s", className + " - " + errorMessage);
         OregonTrail.pressAnyKeyToContinue();
     }
-} 
+}
