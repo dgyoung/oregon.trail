1e47532b9dfae88423ebeebb80190d827e05a04a
David <David@QWERTYking-8>
2018-04-11 18:55:02 -0600

    Merge origin/master
    Conflicts:
    	oregonTrail/src/byui/cit260/oregontrail/view/ReportView.java

--- oregonTrail/src/byui/cit260/oregontrail/control/HuntControl.java
+++ oregonTrail/src/byui/cit260/oregontrail/control/HuntControl.java
@@ -8,52 +8,5 @@ import java.util.Scanner;
  * @author David
  */
 public class HuntControl {
-    public static int hunt() throws InterruptedException {
-        int count = 0;
-        int meat = 0;
-        do {
-            int delay = (int) (Math.random() * 5000);
-            int selecter = (int) Math.ceil(Math.random() * 3);
-            String word = null;
-            long time = 0;
-            switch (selecter) {
-                case 1:
-                    word = "POW";
-                    System.out.println(word);
-                    time = System.currentTimeMillis();
-                    meat += getInput(word, time, delay);
-                    break;
-                case 2:
-                    word = "BANG";
-                    System.out.println(word);
-                    time = System.currentTimeMillis();
-                    meat += getInput(word, time, delay);
-                    break;
-                case 3:
-                    word = "FIRE";
-                    System.out.println(word);
-                    time = System.currentTimeMillis();
-                    meat += getInput(word, time, delay);
-                    break;
-                default:
-            }
-
-            count++;
-        } while (count < 10);
-        return meat;
-    }
-
-    public static int getInput(String word, long time, long delay) {
-        Scanner keyboard = new Scanner(System.in);
-        String input = null;
-        input = keyboard.nextLine().trim().toUpperCase();
-        if (input.equals(word) && (time + delay) > System.currentTimeMillis()) {
-            System.out.print("Hit\n");
-            return 25;
-        } else {
-            System.out.print("Miss\n");
-            return 0;
-        }
-
-    }
+    
 }
--- oregonTrail/src/byui/cit260/oregontrail/view/HuntingView.java
+++ oregonTrail/src/byui/cit260/oregontrail/view/HuntingView.java
@@ -3,6 +3,7 @@ package byui.cit260.oregontrail.view;
 import byui.cit260.oregontrail.control.GameControl;
 import byui.cit260.oregontrail.control.HuntControl;
 import byui.cit260.oregontrail.model.Player;
+import java.util.Scanner;
 
 public class HuntingView extends View {
 
@@ -28,7 +29,7 @@ public class HuntingView extends View {
         switch (choice) {
             case "R": {
                 try {
-                    HuntControl.hunt();
+                    hunt();
                 } catch (InterruptedException ex) {
                     Thread.currentThread().interrupt();
                 }
@@ -39,6 +40,54 @@ public class HuntingView extends View {
                                   "Enter a valid option");
         }
         return false;
+    }
+    public int hunt() throws InterruptedException {
+        int count = 0;
+        int meat = 0;
+        do {
+            int delay = (int) (Math.random() * 5000);
+            int selecter = (int) Math.ceil(Math.random() * 3);
+            String word = null;
+            long time = 0;
+            switch (selecter) {
+                case 1:
+                    word = "POW";
+                    this.console.println(word);
+                    time = System.currentTimeMillis();
+                    meat += getInput(word, time, delay);
+                    break;
+                case 2:
+                    word = "BANG";
+                    this.console.println(word);
+                    time = System.currentTimeMillis();
+                    meat += getInput(word, time, delay);
+                    break;
+                case 3:
+                    word = "FIRE";
+                    this.console.println(word);
+                    time = System.currentTimeMillis();
+                    meat += getInput(word, time, delay);
+                    break;
+                default:
+            }
+
+            count++;
+        } while (count < 10);
+        return meat;
+    }
+
+    public int getInput(String word, long time, long delay) {
+        Scanner keyboard = new Scanner(System.in);
+        String input = null;
+        input = keyboard.nextLine().trim().toUpperCase();
+        if (input.equals(word) && (time + delay) > System.currentTimeMillis()) {
+            this.console.print("Hit\n");
+            return 25;
+        } else {
+            this.console.print("Miss\n");
+            return 0;
+        }
+
     }
     
 }
